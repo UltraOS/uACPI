4ea1b1d280f7e6d547eeccb5b16fc4d9a73fa3d1
8infy <99danilt@gmail.com>
2022-11-07 19:48:10 +0300

    Add a public 'types.h' header
    This simply exports whatever is in the platform/types.h and adds a few
    of its own typedefs on top.

---
+++ include/uacpi/types.h
@@ -0,0 +1,11 @@
+#include <uacpi/platform/types.h>
+#include <uacpi/platform/compiler.h>
+
+#if UACPI_POINTER_SIZE == 4 && defined(UACPI_PHYS_ADDR_IS_32BITS)
+typedef uint32_t uacpi_phys_addr;
+typedef uint32_t uacpi_io_addr;
+#else
+typedef uint64_t uacpi_phys_addr;
+typedef uint64_t uacpi_io_addr;
+#endif
+
