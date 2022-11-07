c685cbb1fb9a83b01fdcde299c854fc96cd98cd2
8infy <99danilt@gmail.com>
2022-11-07 20:16:27 +0300

    Add a 'log.h' header
    This conveniently wraps the kernel logging API with additional checking
    related to the current log level as set in the runtime context.

---
+++ include/uacpi/internal/log.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <uacpi/kernel_api.h>
+#include <uacpi/internal/context.h>
+
+#define uacpi_log_lvl(lvl, ...) \
+    do { if (uacpi_rt_should_log(lvl)) uacpi_kernel_log(lvl, __VA_ARGS__); } while (0)
+
+#define uacpi_trace(...) uacpi_log_lvl(UACPI_LOG_TRACE, __VA_ARGS__)
+#define uacpi_info(...)  uacpi_log_lvl(UACPI_LOG_INFO, __VA_ARGS__)
+#define uacpi_warn(...)  uacpi_log_lvl(UACPI_LOG_WARN, __VA_ARGS__)
+#define uacpi_error(...) uacpi_log_lvl(UACPI_LOG_ERROR, __VA_ARGS__)
+#define uacpi_fatal(...) uacpi_log_lvl(UACPI_LOG_FATAL, __VA_ARGS__)
