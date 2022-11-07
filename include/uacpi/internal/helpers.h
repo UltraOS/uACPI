109bf596415fefb0149aa87f1342cab648abf5dc
8infy <99danilt@gmail.com>
2022-11-07 19:53:53 +0300

    internal/helpers: STATIC_ASSERT -> UACPI_STATIC_ASSERT
    Prefix with UACPI_ so that we don't accidentally collide with a
    used-defined macro of the same name.

--- include/uacpi/internal/helpers.h
+++ include/uacpi/internal/helpers.h
@@ -5,12 +5,12 @@
 #define UACPI_UNUSED(x) (void)(x)
 
 #ifdef __cplusplus
-#define STATIC_ASSERT static_assert
+#define UACPI_STATIC_ASSERT static_assert
 #else
-#define STATIC_ASSERT _Static_assert
+#define UACPI_STATIC_ASSERT _Static_assert
 #endif
 
-#define UACPI_BUILD_BUG_ON_WITH_MSG(expr, msg) STATIC_ASSERT(!(expr), msg)
+#define UACPI_BUILD_BUG_ON_WITH_MSG(expr, msg) UACPI_STATIC_ASSERT(!(expr), msg)
 
 #define UACPI_BUILD_BUG_ON(expr) \
     UACPI_BUILD_BUG_ON_WITH_MSG(expr, "BUILD BUG: " #expr " evaluated to true")
