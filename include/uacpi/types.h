3c357778abe64675a9bade748262137794370181
8infy <99danilt@gmail.com>
2022-11-14 00:10:54 +0300

    types: use uacpi-specific types for address typedefs

--- include/uacpi/types.h
+++ include/uacpi/types.h
@@ -3,10 +3,10 @@
 #include <uacpi/platform/compiler.h>
 
 #if UACPI_POINTER_SIZE == 4 && defined(UACPI_PHYS_ADDR_IS_32BITS)
-typedef uint32_t uacpi_phys_addr;
-typedef uint32_t uacpi_io_addr;
+typedef uacpi_u32 uacpi_phys_addr;
+typedef uacpi_u32 uacpi_io_addr;
 #else
-typedef uint64_t uacpi_phys_addr;
-typedef uint64_t uacpi_io_addr;
+typedef uacpi_u64 uacpi_phys_addr;
+typedef uacpi_u64 uacpi_io_addr;
 #endif
 
