1c0045d95b7a6c89fa5d860f738d9b3ccf8f1e63
Daniil Tatianin <99danilt@gmail.com>
2023-11-13 22:10:13 +0300

    stdlib: add basic UACPI_MIN/UACPI_MAX macros
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/stdlib.h
+++ include/uacpi/internal/stdlib.h
@@ -3,3 +3,7 @@
 #include <uacpi/platform/stdlib.h>
 
 #define uacpi_memzero(ptr, size) uacpi_memset(ptr, 0, size)
+
+#define UACPI_COMPARE(x, y, op) ((x) op (y) ? (x) : (y))
+#define UACPI_MIN(x, y) UACPI_COMPARE(x, y, <)
+#define UACPI_MAX(x, y) UACPI_COMPARE(x, y, >)
