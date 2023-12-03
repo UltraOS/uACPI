c3f062e87239a0d2a73ae22690a8f86262807ac4
Daniil Tatianin <99danilt@gmail.com>
2023-12-04 00:25:06 +0300

    stdlib: make uacpi_memcpy_zerout safer
    Allow src_size > dst_size. This also fixes a bug where we would zero
    memory at dst offset 0 instead of offsetting by the size of source.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- source/stdlib.c
+++ source/stdlib.c
@@ -3,9 +3,11 @@
 void uacpi_memcpy_zerout(void *dst, const void *src,
                          uacpi_size dst_size, uacpi_size src_size)
 {
-    if (src_size)
-        uacpi_memcpy(dst, src, src_size);
+    uacpi_size bytes_to_copy = UACPI_MIN(src_size, dst_size);
 
-    if (dst_size > src_size)
-        uacpi_memzero(dst, dst_size - src_size);
+    if (bytes_to_copy)
+        uacpi_memcpy(dst, src, bytes_to_copy);
+
+    if (dst_size > bytes_to_copy)
+        uacpi_memzero((uacpi_u8*)dst + bytes_to_copy, dst_size - bytes_to_copy);
 }
