f4f7c23de1d8863caff5e26900df51cafcf25d18
Daniil Tatianin <99danilt@gmail.com>
2023-12-09 15:05:41 +0300

    opcodes: use EXISTNG_NAMESTRING_OR_NULL when parsing a namestring
    So that opcodes that support unresolved names have a chance of handling
    this case.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/opcodes.h
+++ include/uacpi/internal/opcodes.h
@@ -251,7 +251,7 @@ const struct uacpi_op_spec *uacpi_get_op_spec(uacpi_aml_op);
         UACPI_InternalOpUnresolvedNameString_##character, code, \
         {                                                       \
             UACPI_PARSE_OP_AML_PC_DECREMENT,                    \
-            UACPI_PARSE_OP_EXISTING_NAMESTRING,                 \
+            UACPI_PARSE_OP_EXISTNG_NAMESTRING_OR_NULL,          \
             UACPI_PARSE_OP_CONVERT_NAMESTRING,                  \
         },                                                      \
         UACPI_OP_PROPERTY_SIMPLE_NAME |                         \
