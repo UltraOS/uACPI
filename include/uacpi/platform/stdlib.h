608676d225134ca450467b224c44ddd94a2b22ea
Daniil Tatianin <99danilt@gmail.com>
2023-12-10 19:16:59 +0300

    platform/stdlib: introduce uacpi_strtoull
    This will be used for converting strings to integers in the ToInteger
    implementation.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/platform/stdlib.h
+++ include/uacpi/platform/stdlib.h
@@ -6,8 +6,10 @@
  */
 
 #include <string.h>
+#include <stdlib.h>
 
 #define uacpi_memcpy memcpy
 #define uacpi_memset memset
 #define uacpi_memcmp memcmp
 #define uacpi_strnlen strnlen
+#define uacpi_strtoull strtoull
