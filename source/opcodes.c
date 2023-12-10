651912d4c73c2ca67b20a9bf7f41507468ed064f
Daniil Tatianin <99danilt@gmail.com>
2023-12-10 20:58:46 +0300

    opcodes: add missing FatalOp to the extended opcode table
    Every op after Debug would be off-by-one because we were missing FatalOp
    there.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- source/opcodes.c
+++ source/opcodes.c
@@ -14,20 +14,20 @@ static const struct uacpi_op_spec ext_opcode_table[] = {
 #define _(op) (op & 0x00FF)
 
 const static uacpi_u8 ext_op_to_idx[0x100] = {
-    [_(UACPI_AML_OP_MutexOp)]      = 1,  [_(UACPI_AML_OP_EventOp)]       = 2,
-    [_(UACPI_AML_OP_CondRefOfOp)]  = 3,  [_(UACPI_AML_OP_CreateFieldOp)] = 4,
-    [_(UACPI_AML_OP_LoadTableOp)]  = 5,  [_(UACPI_AML_OP_LoadOp)]        = 6,
-    [_(UACPI_AML_OP_StallOp)]      = 7,  [_(UACPI_AML_OP_SleepOp)]       = 8,
-    [_(UACPI_AML_OP_AcquireOp)]    = 9,  [_(UACPI_AML_OP_SignalOp)]      = 10,
-    [_(UACPI_AML_OP_WaitOp)]       = 11, [_(UACPI_AML_OP_ResetOp)]       = 12,
-    [_(UACPI_AML_OP_ReleaseOp)]    = 13, [_(UACPI_AML_OP_FromBDCOp)]     = 14,
-    [_(UACPI_AML_OP_ToBCD)]        = 15, [_(UACPI_AML_OP_RevisionOp)]    = 16,
-    [_(UACPI_AML_OP_DebugOp)]      = 17, [_(UACPI_AML_OP_TimerOp)]       = 18,
-    [_(UACPI_AML_OP_OpRegionOp)]   = 19, [_(UACPI_AML_OP_FieldOp)]       = 20,
-    [_(UACPI_AML_OP_DeviceOp)]     = 21, [_(UACPI_AML_OP_ProcessorOp)]   = 22,
-    [_(UACPI_AML_OP_PowerResOp)]   = 23, [_(UACPI_AML_OP_ThermalZoneOp)] = 24,
-    [_(UACPI_AML_OP_IndexFieldOp)] = 25, [_(UACPI_AML_OP_BankFieldOp)]   = 26,
-    [_(UACPI_AML_OP_DataRegionOp)] = 27,
+    [_(UACPI_AML_OP_MutexOp)]       = 1,  [_(UACPI_AML_OP_EventOp)]       = 2,
+    [_(UACPI_AML_OP_CondRefOfOp)]   = 3,  [_(UACPI_AML_OP_CreateFieldOp)] = 4,
+    [_(UACPI_AML_OP_LoadTableOp)]   = 5,  [_(UACPI_AML_OP_LoadOp)]        = 6,
+    [_(UACPI_AML_OP_StallOp)]       = 7,  [_(UACPI_AML_OP_SleepOp)]       = 8,
+    [_(UACPI_AML_OP_AcquireOp)]     = 9,  [_(UACPI_AML_OP_SignalOp)]      = 10,
+    [_(UACPI_AML_OP_WaitOp)]        = 11, [_(UACPI_AML_OP_ResetOp)]       = 12,
+    [_(UACPI_AML_OP_ReleaseOp)]     = 13, [_(UACPI_AML_OP_FromBDCOp)]     = 14,
+    [_(UACPI_AML_OP_ToBCD)]         = 15, [_(UACPI_AML_OP_RevisionOp)]    = 16,
+    [_(UACPI_AML_OP_DebugOp)]       = 17, [_(UACPI_AML_OP_FatalOp)]       = 18,
+    [_(UACPI_AML_OP_TimerOp)]       = 19, [_(UACPI_AML_OP_OpRegionOp)]    = 20,
+    [_(UACPI_AML_OP_FieldOp)]       = 21, [_(UACPI_AML_OP_DeviceOp)]      = 22,
+    [_(UACPI_AML_OP_ProcessorOp)]   = 23, [_(UACPI_AML_OP_PowerResOp)]    = 24,
+    [_(UACPI_AML_OP_ThermalZoneOp)] = 25, [_(UACPI_AML_OP_IndexFieldOp)]  = 26,
+    [_(UACPI_AML_OP_BankFieldOp)]   = 27, [_(UACPI_AML_OP_DataRegionOp)]  = 28,
 };
 
 const struct uacpi_op_spec *uacpi_get_op_spec(uacpi_aml_op op)
