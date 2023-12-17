8ceaa7373d00987eab89154acf1b56753ed2134f
Daniil Tatianin <99danilt@gmail.com>
2023-12-17 21:53:57 +0300

    Move uacpi_control_method to the types header
    A control method is not any sort of special object type, so there's no
    reason for it to exist in a separate header away from everyone else.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/interpreter.h
+++ include/uacpi/internal/interpreter.h
@@ -4,15 +4,6 @@
 #include <uacpi/status.h>
 #include <uacpi/internal/namespace.h>
 
-struct uacpi_control_method {
-    uacpi_u8 *code;
-    uacpi_u32 size;
-    uacpi_u8 sync_level : 4;
-    uacpi_u8 args : 3;
-    uacpi_u8 is_serialized : 1;
-    uacpi_u8 named_objects_persist: 1;
-};
-
 uacpi_status uacpi_execute_control_method(uacpi_namespace_node *scope,
                                           uacpi_control_method *method,
                                           uacpi_args *args, uacpi_object **ret);
