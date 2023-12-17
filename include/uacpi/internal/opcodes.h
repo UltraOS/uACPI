3b1ac65d9e1eff22c3a1f77d1fc182b6723d66d8
Daniil Tatianin <99danilt@gmail.com>
2023-12-17 14:59:29 +0300

    Implement Timer
    This uses the recently introduced uacpi_kernel_get_ticks helper and
    simply propagates the value returned by it.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/opcodes.h
+++ include/uacpi/internal/opcodes.h
@@ -1148,7 +1148,10 @@ UACPI_OP(                                                   \
 UACPI_OP(                                                   \
     TimerOp, UACPI_EXT_OP(0x33),                            \
     {                                                       \
-        UACPI_PARSE_OP_TODO,                                \
+        UACPI_PARSE_OP_OBJECT_ALLOC_TYPED,                  \
+            UACPI_OBJECT_INTEGER,                           \
+        UACPI_PARSE_OP_INVOKE_HANDLER,                      \
+        UACPI_PARSE_OP_OBJECT_TRANSFER_TO_PREV,             \
     },                                                      \
     UACPI_OP_PROPERTY_TERM_ARG                              \
 )                                                           \
