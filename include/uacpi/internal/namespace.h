67a3ada2712b7eac69350a16864c3035a1861550
Daniil Tatianin <99danilt@gmail.com>
2023-12-23 23:12:54 +0300

    Introduce public namespace API
    This splits the internal namespace header in two, essentially making
    some of the previously private API public.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/namespace.h
+++ include/uacpi/internal/namespace.h
@@ -14,25 +14,6 @@ typedef struct uacpi_namespace_node {
 
 uacpi_status uacpi_namespace_initialize_predefined(void);
 
-uacpi_namespace_node *uacpi_namespace_root(void);
-
-enum uacpi_predefined_namespace {
-    UACPI_PREDEFINED_NAMESPACE_ROOT = 0,
-    UACPI_PREDEFINED_NAMESPACE_GPE,
-    UACPI_PREDEFINED_NAMESPACE_PR,
-    UACPI_PREDEFINED_NAMESPACE_SB,
-    UACPI_PREDEFINED_NAMESPACE_SI,
-    UACPI_PREDEFINED_NAMESPACE_TZ,
-    UACPI_PREDEFINED_NAMESPACE_GL,
-    UACPI_PREDEFINED_NAMESPACE_OS,
-    UACPI_PREDEFINED_NAMESPACE_OSI,
-    UACPI_PREDEFINED_NAMESPACE_REV,
-    UACPI_PREDEFINED_NAMESPACE_MAX = UACPI_PREDEFINED_NAMESPACE_REV,
-};
-uacpi_namespace_node *uacpi_namespace_get_predefined(
-    enum uacpi_predefined_namespace
-);
-
 uacpi_namespace_node *uacpi_namespace_node_alloc(uacpi_object_name name);
 void uacpi_namespace_node_free(uacpi_namespace_node *node);
 
@@ -43,5 +24,3 @@ uacpi_namespace_node *uacpi_namespace_node_find_sub_node(
     uacpi_namespace_node *parent,
     uacpi_object_name name
 );
-
-uacpi_object *uacpi_namespace_node_get_object(uacpi_namespace_node *node);
