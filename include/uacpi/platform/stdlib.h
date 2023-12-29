44d9ec278e75376d085553ed54bcd8381bdea452
Daniil Tatianin <99danilt@gmail.com>
2023-12-29 15:51:22 +0300

    platform/stdlib: introduce uacpi_memmove
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/platform/stdlib.h
+++ include/uacpi/platform/stdlib.h
@@ -17,6 +17,7 @@
 #define uacpi_memcpy memcpy
 #define uacpi_memset memset
 #define uacpi_memcmp memcmp
+#define uacpi_memmove memmove
 #define uacpi_strnlen strnlen
 #define uacpi_strlen strlen
 #define uacpi_strtoull strtoull
