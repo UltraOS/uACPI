df3f0a44872d0aa1d05c14e5f170ee6f59a1c13c
Daniil Tatianin <99danilt@gmail.com>
2024-01-12 21:41:45 +0300

    Implement _OSI
    This adds bare bones support for _OSI that has a hardcoded list of
    supported features.
    
    An obvious TODO to make this dynamic in the future.
    
    The fact that it had to be implemented in interpreter.c is also a FIXME
    of course, but this is currently the only way that could be done because
    all of the interpreter structures and helpers are private & local.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/interpreter.h
+++ include/uacpi/internal/interpreter.h
@@ -6,6 +6,7 @@
 #include <uacpi/internal/tables.h>
 
 uacpi_status uacpi_load_table(struct uacpi_table*);
+uacpi_status uacpi_osi(uacpi_handle handle, uacpi_object *retval);
 
 uacpi_status uacpi_execute_control_method(uacpi_namespace_node *scope,
                                           uacpi_control_method *method,
