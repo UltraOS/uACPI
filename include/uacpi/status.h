6f12cfcfabb5af40c86ad5dd4a0f93ee202fceea
Daniil Tatianin <99danilt@gmail.com>
2024-01-13 19:09:40 +0300

    Introduce UACPI_STATUS_AML_SYNC_LEVEL_TOO_HIGH
    This error code is used to report an error where AML attempts to acquire
    a mutex or invoke a serialized method with a sync level that is lower
    than current.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/status.h
+++ include/uacpi/status.h
@@ -29,6 +29,7 @@ typedef enum uacpi_status {
     UACPI_STATUS_AML_INCOMPATIBLE_OBJECT_TYPE = 0xEFFF0004,
     UACPI_STATUS_AML_BAD_ENCODING = 0xEFFF0005,
     UACPI_STATUS_AML_OUT_OF_BOUNDS_INDEX = 0xEFFF0006,
+    UACPI_STATUS_AML_SYNC_LEVEL_TOO_HIGH = 0xEFFF0007,
 } uacpi_status;
 
 const char *uacpi_status_to_string(uacpi_status);
