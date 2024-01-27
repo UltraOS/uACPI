f379251be9aa19576cefb2013c0ed26a56e24d6f
Daniil Tatianin <99danilt@gmail.com>
2024-01-28 00:36:33 +0300

    opregion: introduce a uacpi_trace_region_io helper
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/opregion.h
+++ include/uacpi/internal/opregion.h
@@ -6,6 +6,10 @@
 void uacpi_trace_region_error(
     uacpi_namespace_node *node, uacpi_char *message, uacpi_status ret
 );
+void uacpi_trace_region_io(
+    uacpi_namespace_node *node, uacpi_region_op op,
+    uacpi_u64 offset, uacpi_u8 byte_size, uacpi_u64 ret
+);
 
 void uacpi_opregion_uninstall_handler(uacpi_namespace_node *node);
 
