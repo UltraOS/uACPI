e86d4c64553cc3d3aaf15cec5f063f0e92975305
Daniil Tatianin <99danilt@gmail.com>
2024-01-28 17:41:52 +0300

    types: uacpi_address_space_to_string: use specification names
    Specification names are more readable and easier to identify.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- source/types.c
+++ source/types.c
@@ -55,29 +55,29 @@ const uacpi_char *uacpi_address_space_to_string(
 {
     switch (space) {
     case UACPI_ADDRESS_SPACE_SYSTEM_MEMORY:
-        return "System Memory";
+        return "SystemMemory";
     case UACPI_ADDRESS_SPACE_SYSTEM_IO:
-        return "System IO";
+        return "SystemIO";
     case UACPI_ADDRESS_SPACE_PCI_CONFIG:
-        return "PCI Config";
+        return "PCI_Config";
     case UACPI_ADDRESS_SPACE_EMBEDDED_CONTROLLER:
-        return "Embedded Controller";
+        return "EmbeddedControl";
     case UACPI_ADDRESS_SPACE_SMBUS:
         return "SMBus";
     case UACPI_ADDRESS_SPACE_SYSTEM_CMOS:
-        return "CMOS";
+        return "SystemCMOS";
     case UACPI_ADDRESS_SPACE_PCI_BAR_TARGET:
-        return "PCI BAR";
+        return "PciBarTarget";
     case UACPI_ADDRESS_SPACE_IPMI:
         return "IPMI";
     case UACPI_ADDRESS_SPACE_GENERAL_PURPOSE_IO:
-        return "GPIO";
+        return "GeneralPurposeIO";
     case UACPI_ADDRESS_SPACE_GENERIC_SERIAL_BUS:
-        return "Generic Serial Bus";
+        return "GenericSerialBus";
     case UACPI_ADDRESS_SPACE_PCC:
         return "PCC";
     case UACPI_ADDRESS_SPACE_TABLE_DATA:
-        return "Table Data";
+        return "TableData";
     default:
         return "<vendor specific>";
     }
