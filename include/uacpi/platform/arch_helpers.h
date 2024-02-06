1fcbdd6101c05928fe95a537705591df83325cfe
Daniil Tatianin <99danilt@gmail.com>
2024-02-06 19:21:56 +0300

    platform: new arch_helpers header & UACPI_ARCH_FLUSH_CPU_CACHE macro
    This header will contain arch-specific macros and constants.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

---
+++ include/uacpi/platform/arch_helpers.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef UACPI_OVERRIDE_ARCH_HELPERS
+#include "uacpi_arch_helpers.h"
+#else
+
+#ifndef UACPI_ARCH_FLUSH_CPU_CACHE
+#define UACPI_ARCH_FLUSH_CPU_CACHE() do {} while (0)
+#endif
+
+#endif
