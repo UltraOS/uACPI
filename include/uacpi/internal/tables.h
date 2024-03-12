90ff9e9da87947af5e9cc911cea37541b0ce23e5
Daniil Tatianin <99danilt@gmail.com>
2024-03-12 23:21:19 +0300

    tables: make most API public, don't store tables in runtime context
    This makes most table API public, more specifically everything related
    to searching. The table array is no longer stored in the global runtime
    context because there's no reason to store it there over just a static
    variable internal to tables.c
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/tables.h
+++ include/uacpi/internal/tables.h
@@ -3,26 +3,9 @@
 #include <uacpi/internal/context.h>
 #include <uacpi/types.h>
 #include <uacpi/status.h>
+#include <uacpi/tables.h>
 
-enum uacpi_table_type {
-    UACPI_TABLE_TYPE_FADT = 0,
-    UACPI_TABLE_TYPE_DSDT = 1,
-    UACPI_TABLE_TYPE_SSDT = 2,
-    UACPI_TABLE_TYPE_INVALID = 0xFFFF,
-};
-
-// FADT + DSDT have a hardcoded index in the array
-#define UACPI_BASE_TABLE_COUNT 2
-
-struct uacpi_table_identifiers {
-    uacpi_object_name signature;
-
-    // if oemid[0] == 0 this field is ignored
-    char oemid[6];
-
-    // if oem_table_id[0] == 0 this field is ignored
-    char oem_table_id[8];
-};
+uacpi_status uacpi_initialize_tables(void);
 
 uacpi_status uacpi_table_append(uacpi_phys_addr addr,
                                 struct uacpi_table **out_table);
@@ -30,27 +13,3 @@ uacpi_status uacpi_table_append(uacpi_phys_addr addr,
 uacpi_status
 uacpi_table_append_mapped(uacpi_virt_addr virt_addr,
                           struct uacpi_table **out_table);
-
-uacpi_status
-uacpi_table_find_by_type(enum uacpi_table_type type,
-                         struct uacpi_table **out_table);
-
-
-uacpi_status
-uacpi_table_find_by_signature(uacpi_object_name signature,
-                              struct uacpi_table **out_table);
-
-uacpi_status
-uacpi_table_find_next_with_same_signature(struct uacpi_table **in_out_table);
-
-uacpi_status
-uacpi_table_find(struct uacpi_table_identifiers *id,
-                 struct uacpi_table **out_table);
-
-/*
- * Returns the pointer to a sanitized internal version of FADT.
- *
- * The revision is guaranteed to be correct. All of the registers are converted
- * to GAS format. Fields that might contain garbage are cleared.
- */
-uacpi_status uacpi_table_fadt(struct acpi_fadt**);
---
+++ include/uacpi/tables.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <uacpi/types.h>
+#include <uacpi/status.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+* Table is valid and may be used
+*/
+#define UACPI_TABLE_VALID (1 << 0)
+
+/*
+ * Table is already mapped and 'virt_addr' may be used to read it
+ */
+#define UACPI_TABLE_MAPPED (1 << 1)
+
+/*
+ * (Only relevant for definition blocks)
+ * The table has already been executed by the interpreter.
+ */
+#define UACPI_TABLE_LOADED (1 << 2)
+
+typedef struct uacpi_table {
+    struct uacpi_shareable shareable;
+    uacpi_object_name signature;
+    uacpi_phys_addr phys_addr;
+    union {
+        uacpi_virt_addr virt_addr;
+        struct acpi_sdt_hdr *hdr;
+    };
+    uacpi_u32 length;
+    uacpi_u8 flags;
+} uacpi_table;
+
+typedef enum uacpi_table_type {
+    UACPI_TABLE_TYPE_FADT = 0,
+    UACPI_TABLE_TYPE_DSDT = 1,
+    UACPI_TABLE_TYPE_SSDT = 2,
+    UACPI_TABLE_TYPE_INVALID = 0xFFFF,
+} uacpi_table_type;
+
+typedef struct uacpi_table_identifiers {
+    uacpi_object_name signature;
+
+    // if oemid[0] == 0 this field is ignored
+    char oemid[6];
+
+    // if oem_table_id[0] == 0 this field is ignored
+    char oem_table_id[8];
+} uacpi_table_identifiers;
+
+uacpi_status
+uacpi_table_find_by_type(uacpi_table_type type,
+                         uacpi_table **out_table);
+
+
+uacpi_status
+uacpi_table_find_by_signature(uacpi_object_name signature,
+                              uacpi_table **out_table);
+
+uacpi_status
+uacpi_table_find_next_with_same_signature(uacpi_table **in_out_table);
+
+uacpi_status
+uacpi_table_find(uacpi_table_identifiers *id, uacpi_table **out_table);
+
+/*
+ * Returns the pointer to a sanitized internal version of FADT.
+ *
+ * The revision is guaranteed to be correct. All of the registers are converted
+ * to GAS format. Fields that might contain garbage are cleared.
+ */
+uacpi_status uacpi_table_fadt(struct acpi_fadt**);
+
+#ifdef __cplusplus
+}
+#endif
