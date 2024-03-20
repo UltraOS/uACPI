0a412a6350c997ffa89e2d9ea45d34bbc6d89c86
Daniil Tatianin <99danilt@gmail.com>
2024-03-20 22:48:50 +0300

    utilities: introduce uacpi_string_to_integer
    This introduces a new helper for converting strings to numbers.
    
    Previously we would depend on a host-provided strtoull implementation
    for this, but this is a not a commonly seen function that kernels
    implement and its accuracy is crucial for making sure GPE matching and
    AML ToInteger operator work correctly.
    
    Because of this, let's ship our own independent implementation of string
    conversions that we have verified to do what we want at a decent
    performance level.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/utilities.h
+++ include/uacpi/internal/utilities.h
@@ -28,6 +28,17 @@ uacpi_status uacpi_check_tbl_signature_with_warn(void*, const char *expect);
  */
 void uacpi_eisa_id_to_string(uacpi_u32, uacpi_char *out_string);
 
+enum uacpi_base {
+    UACPI_BASE_AUTO,
+    UACPI_BASE_OCT = 8,
+    UACPI_BASE_DEC = 10,
+    UACPI_BASE_HEX = 16,
+};
+uacpi_status uacpi_string_to_integer(
+    const uacpi_char *str, uacpi_size max_chars, enum uacpi_base base,
+    uacpi_u64 *out_value
+);
+
 uacpi_status uacpi_eval_hid(uacpi_namespace_node*, uacpi_char **out_hid);
 uacpi_status uacpi_eval_cid(uacpi_namespace_node*, uacpi_pnp_id_list *out_list);
 uacpi_status uacpi_eval_sta(uacpi_namespace_node*, uacpi_u32 *flags);
