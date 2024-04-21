83e00cb8884162b897a1d838301f7a897fe47db6
Daniil Tatianin <99danilt@gmail.com>
2024-04-21 13:06:17 +0300

    Introduce UACPI_LOG_DEBUG
    This will be used for tracking down hangs/lockups and is the most
    verbose log level uACPI provides.
    
    As a bonus, this also adds detailed descriptions of what's expected from
    every other log level.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/log.h
+++ include/uacpi/internal/log.h
@@ -6,6 +6,7 @@
 #define uacpi_log_lvl(lvl, ...) \
     do { if (uacpi_rt_should_log(lvl)) uacpi_kernel_log(lvl, __VA_ARGS__); } while (0)
 
+#define uacpi_debug(...) uacpi_log_lvl(UACPI_LOG_DEBUG, __VA_ARGS__)
 #define uacpi_trace(...) uacpi_log_lvl(UACPI_LOG_TRACE, __VA_ARGS__)
 #define uacpi_info(...)  uacpi_log_lvl(UACPI_LOG_INFO, __VA_ARGS__)
 #define uacpi_warn(...)  uacpi_log_lvl(UACPI_LOG_WARN, __VA_ARGS__)
