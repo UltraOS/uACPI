b0ba92a37331cfd74c514e2baf47ffa091371ad3
Daniil Tatianin <99danilt@gmail.com>
2024-05-16 13:58:02 +0300

    uacpi: introduce a public helpers header
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/helpers.h
+++ include/uacpi/internal/helpers.h
@@ -1,20 +1,7 @@
 #pragma once
 
+#include <uacpi/helpers.h>
+
 #define UACPI_ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 #define UACPI_UNUSED(x) (void)(x)
-
-#ifdef __cplusplus
-#define UACPI_STATIC_ASSERT static_assert
-#else
-#define UACPI_STATIC_ASSERT _Static_assert
-#endif
-
-#define UACPI_BUILD_BUG_ON_WITH_MSG(expr, msg) UACPI_STATIC_ASSERT(!(expr), msg)
-
-#define UACPI_BUILD_BUG_ON(expr) \
-    UACPI_BUILD_BUG_ON_WITH_MSG(expr, "BUILD BUG: " #expr " evaluated to true")
-
-#define UACPI_EXPECT_SIZEOF(type, size)                        \
-    UACPI_BUILD_BUG_ON_WITH_MSG(sizeof(type) != size,          \
-                                "BUILD BUG: invalid type size")
