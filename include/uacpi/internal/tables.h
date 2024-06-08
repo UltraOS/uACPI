71da2fd0f7829cec316892d2969a9183be2de184
Daniil Tatianin <99danilt@gmail.com>
2024-06-08 13:35:57 +0300

    Rework the table subsystem to be thread safe & extensible
    The table subsystem was probably the weakest and least featureful link
    in the entire project. This rewrite aims to bring it up to part with the
    rest of the library. In particular, this adds the ability to override,
    deny, and otherwise control the tables that are being loaded at runtime.
    This also adds public helpers for dynamically loading or installing
    tables by the host.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/tables.h
+++ include/uacpi/internal/tables.h
@@ -1,26 +1,62 @@
 #pragma once
 
 #include <uacpi/internal/context.h>
+#include <uacpi/internal/interpreter.h>
 #include <uacpi/types.h>
 #include <uacpi/status.h>
 #include <uacpi/tables.h>
 
+enum uacpi_table_origin {
+    UACPI_TABLE_ORIGIN_FIRMWARE_VIRTUAL = 0,
+    UACPI_TABLE_ORIGIN_FIRMWARE_PHYSICAL,
+
+    UACPI_TABLE_ORIGIN_HOST_VIRTUAL,
+    UACPI_TABLE_ORIGIN_HOST_PHYSICAL,
+};
+
+struct uacpi_installed_table {
+    uacpi_object_name signature;
+    uacpi_phys_addr phys_addr;
+    union {
+        void *ptr;
+        struct acpi_sdt_hdr *hdr;
+    };
+    uacpi_u32 length;
+
+#define UACPI_TABLE_LOADED (1 << 0)
+    uacpi_u8 flags;
+    uacpi_u8 origin;
+};
+
 uacpi_status uacpi_initialize_tables(void);
 
-uacpi_status uacpi_table_append(uacpi_phys_addr addr,
-                                struct uacpi_table **out_table);
+uacpi_status uacpi_table_install_physical_with_origin(
+    uacpi_phys_addr phys, enum uacpi_table_origin origin, uacpi_table *out_table
+);
+uacpi_status uacpi_table_install_with_origin(
+    void *virt, enum uacpi_table_origin origin, uacpi_table *out_table
+);
 
-uacpi_status
-uacpi_table_append_mapped(uacpi_virt_addr virt_addr,
-                          struct uacpi_table **out_table);
+void uacpi_table_mark_as_loaded(uacpi_size idx);
+
+uacpi_status uacpi_table_load_with_cause(
+    uacpi_size idx, enum uacpi_table_load_cause cause
+);
 
 enum uacpi_table_iteration_decision {
     UACPI_TABLE_ITERATION_DECISION_CONTINUE,
     UACPI_TABLE_ITERATION_DECISION_BREAK,
 };
-typedef enum uacpi_table_iteration_decision
-    (*uacpi_table_iteration_callback)(void *user, uacpi_table *tbl);
+typedef enum uacpi_table_iteration_decision (*uacpi_table_iteration_callback)
+    (void *user, struct uacpi_installed_table *tbl, uacpi_size idx);
 
-void uacpi_for_each_table(
+uacpi_status uacpi_for_each_table(
     uacpi_size base_idx, uacpi_table_iteration_callback, void *user
+);
+
+typedef uacpi_bool (*uacpi_table_match_callback)
+    (struct uacpi_installed_table *tbl);
+
+uacpi_status uacpi_table_match(
+    uacpi_size base_idx, uacpi_table_match_callback, uacpi_table *out_table
 );
--- include/uacpi/tables.h
+++ include/uacpi/tables.h
@@ -7,40 +7,6 @@
 extern "C" {
 #endif
 
-/*
-* Table is valid and may be used
-*/
-#define UACPI_TABLE_VALID (1 << 0)
-
-/*
- * Table is already mapped and 'virt_addr' may be used to read it
- */
-#define UACPI_TABLE_MAPPED (1 << 1)
-
-/*
- * (Only relevant for definition blocks)
- * The table has already been executed by the interpreter.
- */
-#define UACPI_TABLE_LOADED (1 << 2)
-
-typedef struct uacpi_table {
-    struct uacpi_shareable shareable;
-    uacpi_object_name signature;
-    uacpi_phys_addr phys_addr;
-    union {
-        uacpi_virt_addr virt_addr;
-        struct acpi_sdt_hdr *hdr;
-    };
-    uacpi_u32 length;
-    uacpi_u8 flags;
-} uacpi_table;
-
-typedef enum uacpi_table_type {
-    UACPI_TABLE_TYPE_FADT = 0,
-    UACPI_TABLE_TYPE_DSDT = 1,
-    UACPI_TABLE_TYPE_INVALID = 0xFFFF,
-} uacpi_table_type;
-
 typedef struct uacpi_table_identifiers {
     uacpi_object_name signature;
 
@@ -51,20 +17,48 @@ typedef struct uacpi_table_identifiers {
     char oem_table_id[8];
 } uacpi_table_identifiers;
 
-uacpi_status
-uacpi_table_find_by_type(uacpi_table_type type,
-                         uacpi_table **out_table);
+typedef struct uacpi_table {
+    union {
+        uacpi_virt_addr virt_addr;
+        void *ptr;
+        struct acpi_sdt_hdr *hdr;
+    };
 
+     // Index number used to identify this table internally
+    uacpi_size index;
+} uacpi_table;
 
-uacpi_status
-uacpi_table_find_by_signature(const uacpi_char *signature,
-                              uacpi_table **out_table);
+/*
+ * Install a table from either a virtual or a physical address.
+ * The table is simply stored in the internal table array, and not loaded by
+ * the interpreter (see uacpi_table_load).
+ *
+ * The table is optionally returned via 'out_table'.
+ *
+ * Manual calls to uacpi_table_install are not subject to filtering via the
+ * table installation callback (if any).
+ */
+uacpi_status uacpi_table_install(
+    void*, uacpi_table *out_table
+);
+uacpi_status uacpi_table_install_physical(
+    uacpi_phys_addr, uacpi_table *out_table
+);
 
-uacpi_status
-uacpi_table_find_next_with_same_signature(uacpi_table **in_out_table);
+/*
+ * Load a previously installed table by feeding it to the interpreter.
+ */
+uacpi_status uacpi_table_load(uacpi_size index);
 
-uacpi_status
-uacpi_table_find(const uacpi_table_identifiers *id, uacpi_table **out_table);
+uacpi_status uacpi_table_find_by_signature(
+    const uacpi_char *signature, uacpi_table *out_table
+);
+uacpi_status uacpi_table_find_next_with_same_signature(
+    uacpi_table *in_out_table
+);
+uacpi_status uacpi_table_find(
+    const uacpi_table_identifiers *id, uacpi_table *out_table
+);
 
 /*
  * Returns the pointer to a sanitized internal version of FADT.
@@ -73,6 +67,43 @@ uacpi_table_find(const uacpi_table_identifiers *id, uacpi_table **out_table);
  * to GAS format. Fields that might contain garbage are cleared.
  */
 uacpi_status uacpi_table_fadt(struct acpi_fadt**);
+
+typedef enum uacpi_table_installation_disposition {
+    // Allow the table to be installed as-is
+    UACPI_TABLE_INSTALLATION_DISPOSITON_ALLOW = 0,
+
+    /*
+     * Deny the table from being installed completely. This is useful for
+     * debugging various problems, e.g. AML loading bad SSDTs that cause the
+     * system to hang or enter an undesired state.
+     */
+    UACPI_TABLE_INSTALLATION_DISPOSITON_DENY,
+
+    /*
+     * Override the table being installed with the table at the virtual address
+     * returned in 'out_override_address'.
+     */
+    UACPI_TABLE_INSTALLATION_DISPOSITON_VIRTUAL_OVERRIDE,
+
+    /*
+     * Override the table being installed with the table at the physical address
+     * returned in 'out_override_address'.
+     */
+    UACPI_TABLE_INSTALLATION_DISPOSITON_PHYSICAL_OVERRIDE,
+} uacpi_table_installation_disposition;
+
+typedef uacpi_table_installation_disposition (*uacpi_table_installation_handler)
+    (struct acpi_sdt_hdr *hdr, uacpi_u64 *out_override_address);
+
+/*
+ * Set a handler that is invoked for each table before it gets installed.
+ *
+ * Depending on the return value, the table is either allowed to be installed
+ * as-is, denied, or overriden with a new one.
+ */
+uacpi_status uacpi_set_table_installation_handler(
+    uacpi_table_installation_handler handler
+);
 
 #ifdef __cplusplus
 }
