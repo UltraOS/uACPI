e734dec3dd5202a0222b5cbbe55f7d5611fdd556
Daniil Tatianin <99danilt@gmail.com>
2024-09-10 22:48:07 +0300

    Move uacpi_log_level from kernel_api.h to types.h
    Since log level has nothing to do with kernel API, move it to types.h
    where similar enums (e.g. uacpi_init_level) live.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/types.h
+++ include/uacpi/types.h
@@ -31,6 +31,37 @@ typedef enum uacpi_init_level {
     UACPI_INIT_LEVEL_NAMESPACE_INITIALIZED = 3,
 } uacpi_init_level;
 
+typedef enum uacpi_log_level {
+    /*
+     * Super verbose logging, every op & uop being processed is logged.
+     * Mostly useful for tracking down hangs/lockups.
+     */
+    UACPI_LOG_DEBUG = 4,
+
+    /*
+     * A little verbose, every operation region access is traced with a bit of
+     * extra information on top.
+     */
+    UACPI_LOG_TRACE = 3,
+
+    /*
+     * Only logs the bare minimum information about state changes and/or
+     * initialization progress.
+     */
+    UACPI_LOG_INFO  = 2,
+
+    /*
+     * Logs recoverable errors and/or non-important aborts.
+     */
+    UACPI_LOG_WARN  = 1,
+
+    /*
+     * Logs only critical errors that might affect the ability to initialize or
+     * prevent stable runtime.
+     */
+    UACPI_LOG_ERROR = 0,
+} uacpi_log_level;
+
 #if UACPI_POINTER_SIZE == 4 && defined(UACPI_PHYS_ADDR_IS_32BITS)
 typedef uacpi_u32 uacpi_phys_addr;
 typedef uacpi_u32 uacpi_io_addr;
