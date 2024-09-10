03984c14ca14bf43c6c4d60161ffce385eb1ff20
Daniil Tatianin <99danilt@gmail.com>
2024-09-10 22:48:07 +0300

    Move uacpi_log_level from kernel_api.h to types.h
    Since log level has nothing to do with kernel API, move it to types.h
    where similar enums (e.g. uacpi_init_level) live.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/kernel_api.h
+++ include/uacpi/kernel_api.h
@@ -136,37 +136,6 @@ void uacpi_kernel_free(void *mem);
 void uacpi_kernel_free(void *mem, uacpi_size size_hint);
 #endif
 
-typedef enum uacpi_log_level {
-    /*
-     * Super verbose logging, every op & uop being processed is logged.
-     * Mostly useful for tracking down hangs/lockups.
-     */
-    UACPI_LOG_DEBUG = 4,
-
-    /*
-     * A little verbose, every operation region access is traced with a bit of
-     * extra information on top.
-     */
-    UACPI_LOG_TRACE = 3,
-
-    /*
-     * Only logs the bare minimum information about state changes and/or
-     * initialization progress.
-     */
-    UACPI_LOG_INFO  = 2,
-
-    /*
-     * Logs recoverable errors and/or non-important aborts.
-     */
-    UACPI_LOG_WARN  = 1,
-
-    /*
-     * Logs only critical errors that might affect the ability to initialize or
-     * prevent stable runtime.
-     */
-    UACPI_LOG_ERROR = 0,
-} uacpi_log_level;
-
 #ifndef UACPI_FORMATTED_LOGGING
 void uacpi_kernel_log(uacpi_log_level, const uacpi_char*);
 #else
