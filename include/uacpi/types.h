935d7b97583f7c651c14769f3b4c39bc1ff73a33
Daniil Tatianin <99danilt@gmail.com>
2024-09-10 22:51:41 +0300

    Increment all underlying values for uacpi_log_level by one
    Reserving 0 as the default value is convenient and will be utilized in
    the following commits.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/types.h
+++ include/uacpi/types.h
@@ -36,30 +36,30 @@ typedef enum uacpi_log_level {
      * Super verbose logging, every op & uop being processed is logged.
      * Mostly useful for tracking down hangs/lockups.
      */
-    UACPI_LOG_DEBUG = 4,
+    UACPI_LOG_DEBUG = 5,
 
     /*
      * A little verbose, every operation region access is traced with a bit of
      * extra information on top.
      */
-    UACPI_LOG_TRACE = 3,
+    UACPI_LOG_TRACE = 4,
 
     /*
      * Only logs the bare minimum information about state changes and/or
      * initialization progress.
      */
-    UACPI_LOG_INFO  = 2,
+    UACPI_LOG_INFO  = 3,
 
     /*
      * Logs recoverable errors and/or non-important aborts.
      */
-    UACPI_LOG_WARN  = 1,
+    UACPI_LOG_WARN  = 2,
 
     /*
      * Logs only critical errors that might affect the ability to initialize or
      * prevent stable runtime.
      */
-    UACPI_LOG_ERROR = 0,
+    UACPI_LOG_ERROR = 1,
 } uacpi_log_level;
 
 #if UACPI_POINTER_SIZE == 4 && defined(UACPI_PHYS_ADDR_IS_32BITS)
