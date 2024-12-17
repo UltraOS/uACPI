8fee2e910a7cfd72502e55b938de373b954b8073
Daniil Tatianin <99danilt@gmail.com>
2024-12-17 15:29:35 +0300

    event: make fully thread safe
    Protect public interfaces with a mutex. When modifying event handlers,
    make sure to mask them and wait for any in-flight IRQs to finish before
    proceeding with modifications, then unmask after we're done.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/internal/event.h
+++ include/uacpi/internal/event.h
@@ -12,8 +12,8 @@ UACPI_STUB_IF_REDUCED_HARDWARE(
     void uacpi_deinitialize_events(void)
 )
 
-UACPI_ALWAYS_OK_FOR_REDUCED_HARDWARE(
-    uacpi_status uacpi_events_match_post_dynamic_table_load(void)
+UACPI_STUB_IF_REDUCED_HARDWARE(
+    void uacpi_events_match_post_dynamic_table_load(void)
 )
 
 UACPI_ALWAYS_ERROR_FOR_REDUCED_HARDWARE(
